5e126ed57f85e032c06c9e55612c8c2c501edd14
24qureshiut <24qureshiut@hazelwick.org>
2026-09-22 08:50:52 +0000

    22/09/26

---
+++ My Work/Task 14.py
@@ -0,0 +1,4 @@
+LR=int(input("enter a length for a rectangle:"))
+Wr=int(input("enter  the width of a rectangle:"))
+AR=Wr*LR
+print("the area of a rectangle is",AR)
--- My Work/Task 5.py
+++ My Work/Task 5.py
@@ -3,4 +3,4 @@ Wtexture = input("Enter a weird Texture:")
 Snoise = input("Enter a strange noise:")
 Mucname = input("Enter a Name:")
 
-Print("Behold The",Wtexture ,Pattribute "ed" ,makes a terrifying *"Snoise", as it shuffles closer!)
+Print
