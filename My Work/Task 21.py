0c045a19769412f9dcd79ec9fcfd3101be8b7ec8
24qureshiut <24qureshiut@hazelwick.org>
2026-09-25 13:56:23 +0000

    25/09/26

---
+++ My Work/Task 21.py
@@ -0,0 +1,5 @@
+name = input("whats ur name:")
+if name == "Brian":
+    print("you are Brian")
+else:
+    print("you're not Brian")
---
+++ My Work/Task 23.py
@@ -0,0 +1,23 @@
+qadd = input("63+37=")
+if qadd =="100":
+    print("Correct")
+else:
+    print("Wrong")
+qsub = input("38-37=")
+if qsub =="1":
+    print("Correct")
+else:
+    print("Wrong")
+qmult = input("3*4=")
+if qmult =="12":
+    print("Correct")
+else:
+    print("Wrong")
+qdiv = input("55/5=")
+if qdiv =="11":
+    print("Correct")
+else:
+    print("Wrong")
+
+
+
